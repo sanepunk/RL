e47eda95d2b0975fafa076be3684a07edc7144bc
Ojas Patil <129142263+sanepunk@users.noreply.github.com>
2026-08-20 09:55:34 +0530

    Formatted codebase

--- Policy Gradients/Reinforce/parallel-jax/optimizer.py
+++ Policy Gradients/Reinforce/parallel-jax/optimizer.py
@@ -2,18 +2,24 @@ from flax import nnx
 import optax
 from typing import Literal
 
-def return_optimizer(model: nnx.Module, optimizer: Literal['adam', 'adamw', 'adamax', 'sgd', 'rmsprop'], learning_rate: float = 1e-3, gradNorm: float | None = None) -> nnx.Optimizer:
+
+def return_optimizer(
+    model: nnx.Module,
+    optimizer: Literal["adam", "adamw", "adamax", "sgd", "rmsprop"],
+    learning_rate: float = 1e-3,
+    gradNorm: float | None = None,
+) -> nnx.Optimizer:
     # Select the optimizer based on the input argument
     optimizer_map = {
-        'adam': optax.adam(learning_rate),
-        'adamw': optax.adamw(learning_rate),
-        'adamax': optax.adamax(learning_rate),
-        'sgd': optax.sgd(learning_rate),
-        'rmsprop': optax.rmsprop(learning_rate)
+        "adam": optax.adam(learning_rate),
+        "adamw": optax.adamw(learning_rate),
+        "adamax": optax.adamax(learning_rate),
+        "sgd": optax.sgd(learning_rate),
+        "rmsprop": optax.rmsprop(learning_rate),
     }
-    
+
     opt = optimizer_map.get(optimizer, optax.adam(learning_rate))
-    
+
     if gradNorm:
         return nnx.Optimizer(
             model,
@@ -21,12 +27,8 @@ def return_optimizer(model: nnx.Module, optimizer: Literal['adam', 'adamw', 'ada
                 # optax.exponential_decay(learning_rate, decay_rate=0.9, transition_steps=10, transition_begin=1),
                 optax.clip_by_global_norm(gradNorm),
                 optax.normalize_by_update_norm(),
-                opt
+                opt,
             ),
-            wrt=nnx.Param 
+            wrt=nnx.Param,
         )
-    return nnx.Optimizer(
-        model,
-        opt,
-        wrt=nnx.Param
-    )
+    return nnx.Optimizer(model, opt, wrt=nnx.Param)
